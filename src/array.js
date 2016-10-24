86651baa41253546472b662bbd2621bff7dc6dc7
hakob90 <hakob93baghdasaryan@gmail.com>
2016-10-24 13:43:48 +0400

    array additions

---
+++ src/array.js
@@ -0,0 +1,13 @@
+console.log(
+    [2, 4, 6, 8, 10].find(item => item % 2 > 0)
+    // [2, 4, 6, 8, 10].findIndex(item => item % 2 > 0)
+    //item > 8;
+    //item === 8;
+);
+
+console.log(
+    ['Hakob', 'Levon', 'Arpine'].fill(),
+    ['Hakob', 'Levon', 'Arpine'].keys(),
+    ['Hakob', 'Levon', 'Arpine'].values(),
+    ['Hakob', 'Levon', 'Arpine'].entries(),
+);
